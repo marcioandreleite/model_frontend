c60d8d3bc095ac1ea21aee5e4e326b7132c77a8d
marcio <marcioandreleite@gmail.com>
2023-06-07 11:50:05 -0300

    feat : acertando model

---
+++ model_code/js/main.js
@@ -0,0 +1,14 @@
+(function () {
+  if (document.addEventListener) {
+    document.addEventListener("contextmenu", function (e) {
+      e.preventDefault();
+      return false;
+    });
+  } else {
+    document.attachEvent("oncontextmenu", function (e) {
+      e = e || window.event;
+      e.returnValue = false;
+      return false;
+    });
+  }
+})()
